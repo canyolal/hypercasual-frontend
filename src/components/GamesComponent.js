592b2c7f71eb1058ca3a71707230ba92e67a021e
Can Yolal <canyolal16@gmail.com>
2022-11-18 22:55:59 +0300

    Code is cleansed

--- src/components/GamesComponent.js
+++ src/components/GamesComponent.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import Form from 'react-bootstrap/Form'
 import Stack from 'react-bootstrap/Stack'
-import Pagination from 'react-bootstrap/Pagination'
 import {Row,Col,Container} from 'react-bootstrap'
 
 import gameService  from '../services/gameService'
+
 import GameTable from './GameTable'
-import Notification from './Notification';
 import GenreForm from './GenreForm';
 import PublisherForm from './PublisherForm';
 import PageSizeForm from './PageSizeForm';
@@ -15,38 +14,14 @@ import SubscribeGames from './SubscribeGames'
 
 const GamesComponent = () => {
 
-  const [Name, setName] = useState("")
-  const [Genre, setGenre] = useState("")
-  const [Publisher, setPublisher] = useState("")
-  const [metadata,setMetadata] = useState([])
-  const [message , setMessage] = useState(null)
+  const [name, setName] = useState("")
+  const [genre, setGenre] = useState("")
+  const [publisher, setPublisher] = useState("")
   const [pageSize, setPageSize] = useState("")
   const [pageNum, setPageNum] = useState(1)
   const [pageCount, setPageCount] = useState(1)
   
   const [Games, setGames] = useState([])
-
-  var pages =[]
-  const updatePagination = () => {
-    pages = []
-    for (let number = 1; number <= metadata.last_page; number++) {
-      pages.push(
-        <Pagination.Item
-          key={number}
-          active={number === metadata.current_page}
-          onClick={() => paginate(number)}
-        >
-          {number}
-        </Pagination.Item>
-      );
-    }
-  }
-  updatePagination()
-  
-
-  const fortmatResponse = (res) => {
-    return JSON.stringify(res,null, 2);
-  }
 
   const serializeQuery = (queryParams) =>{
     const arr = []
@@ -60,56 +35,38 @@ const GamesComponent = () => {
     return qryString
   }
 
-  const paginate = (number) => {
-    let qry = serializeQuery({page: number, name : Name, genre: Genre, publisher_name: Publisher, page_size: pageSize})
-    gameService.get(`/games?${qry}`)
-    .then(res => {
-      setGames(res.data.games)
-      setMetadata(res.data.metadata)
-    })
-  }
-
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const findGamesWithQuery = () => {
-    let qry =serializeQuery({name: Name, genre: Genre, publisher_name : Publisher, page_size : pageSize})
+    let qry =serializeQuery({name: name, genre: genre, publisher_name : publisher, page_size : pageSize})
     gameService.get(`/games?${qry}`)
     .then(res => {
       setGames(res.data.games)
-      setMetadata(res.data.metadata)
       setPageCount(res.data.metadata.last_page)        
       setPageNum(1)
     }
     )
   }
 
-  useEffect(() => {
-    const getDataByQuery = () => {
-      try {
-        findGamesWithQuery();
-      } catch (err) {
-        setMessage(fortmatResponse(err));
-      }
-  }
-    getDataByQuery()
-  },[Name,Genre,Publisher,pageSize]);
+  useEffect(findGamesWithQuery,[name,genre,publisher,pageSize]);
 
   const changePageNumber = () =>{
-    let qry = serializeQuery({page: pageNum, name : Name, genre: Genre, publisher_name: Publisher, page_size: pageSize})
+    let qry = serializeQuery({page: pageNum, name : name, genre: genre, publisher_name: publisher, page_size: pageSize})
     gameService.get(`/games?${qry}`)
     .then(res => {
       setGames(res.data.games)
-      setMetadata(res.data.metadata)
       setPageCount(res.data.metadata.last_page)        
       setPageNum(pageNum)
     })
   }
-
-  useEffect(() => {
-    changePageNumber()
-  }, [pageNum])
+  
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(changePageNumber, [pageNum])
 
   return(
-    <div class='singlePages'>
-        <h1> Top Publisher's Hypercasual Games </h1>
+    <div className='singlePages'>
+        <br/>
+        <br/>
+        <h1> Top publisher's Hypercasual Games </h1>
         <br />
         <p className='gamesComponent'>
           <span>
@@ -122,12 +79,11 @@ const GamesComponent = () => {
         <br />
 
         <Stack direction='horizontal' gap={2}>
-          <Form.Control className='form-control-sm' placeholder='Game' type='text' value={Name} onChange={(e) => setName(e.target.value)} />
+          <Form.Control className='form-control-sm' placeholder='Game' type='text' value={name} onChange={(e) => setName(e.target.value)} />
           <GenreForm setGenre={setGenre}/>
           <PublisherForm setPublisher={setPublisher} />
         </Stack>
 
-        {message === "loading..." ? null : <Notification message={message} />}
         <br/>
         <GameTable games={Games} />  
         <Container>
