d96470d4e11c4e92e2547ac32c97c1d550f3a3ef
Can Yolal <canyolal16@gmail.com>
2022-11-18 22:56:57 +0300

    Subscribe to Mail list is implemented

--- src/components/SubscribeGames.js
+++ src/components/SubscribeGames.js
@@ -1,20 +1,61 @@
 import {useState} from 'react';
+import gameService  from '../services/gameService'
+import Notification from './Notification';
 
-//TODO endpoint should be added for subscribing
-//and POST request should be handled here
 const SubscribeGames = () => {
-    //const [mail, setEmail] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState(null)
+    const [messageTitle, setMessageTitle] = useState(null)
+    const [messageStatus, setMessageStatus] = useState(null)
 
+    const subscribeToMail = () => {
+        gameService.post(`/subscribe`, {email: email})
+        .then((res) => {
+            if (res.status === 202) {
+                setMessage("Subscribed!")
+                setMessageStatus("success")
+                setMessageTitle("")
+                setTimeout(() => {
+                    setMessage(null)
+                    setMessageStatus(null)
+                    setMessageTitle(null)
+                    }, 3000)
+                }
+        }).catch ( err => {
+            if (err.response.status === 500){
+                setMessageTitle("")
+                setMessage(Object.values(err.response.data.error))
+                setMessageStatus("danger")
+                setTimeout(() => {
+                    setMessage(null)
+                    setMessageStatus(null)
+                    setMessageTitle(null)
+                    }, 3000)
+            } else if (err.response.status === 422) {
+                setMessageTitle("")
+                setMessage(Object.values(err.response.data.error))
+                setMessageStatus("warning")
+                setTimeout(() => {
+                    setMessage(null)
+                    setMessageStatus(null)
+                    setMessageTitle(null)
+                    }, 3000)
+            }
+        })
+    }
     return (
         <div className="subscribeGames">
+            <Notification message={message} title={messageTitle} status={messageStatus}/>
             <input className='subscribeToGames'
                 type="e-mail"
                 placeholder=" e-mail"
+                onChange={(e) => setEmail(e.target.value)}
             />
             <button 
                 className="subscribeToGames"
+                onClick={subscribeToMail}
                 >
-                <i class="fas fa-check"></i>  Subscribe
+                <i className="fas fa-check"></i>  Subscribe
             </button>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import {QueryClient, QueryClientProvider} from 'react-query';
-const queryClient = new QueryClient();
-
 ReactDOM.render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
             <App />
-        </QueryClientProvider>
     </React.StrictMode>,
-    document.getElementById('root'))
+    document.getElementById('root')
+)
